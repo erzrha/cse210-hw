01bc7a000a0e9d3438f7e0446a0596a967dada8e
erzrha <eatobrown@hotmail.com>
2023-01-07 20:56:58 +0000

    Finished Prep2 Assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade? ");
+        string ans = Console.ReadLine();
+         int grade = int.Parse(ans);
+
+         string grades = "";
+        if (grade >= 90)
+        {
+            grades = "A";
+        }
+
+        else if (grade >=80 && grade <90)
+        {
+            grades = "B";
+        }
+        else if (grade >= 70 && grade <80)
+        {
+            grades = "C";
+        }
+        else if (grade >= 60 && grade <70)
+        {
+           grades = "D";
+        }
+        else{
+            grades = "F";
+        }
+
+        Console.WriteLine($"Your grades is {grades}");
+        if (grades == "D" || grades == "F")
+        {
+            Console.WriteLine("Don't give up...Better Luck next time");
+        }
+        else
+        {
+            Console.WriteLine("Congratulations, you made it!");
+        }
     }
 }
