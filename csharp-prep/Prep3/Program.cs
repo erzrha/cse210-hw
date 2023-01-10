dd7532b8a4976ad8ae16db3a06b60eb780f429b1
erzrha <eatobrown@hotmail.com>
2023-01-10 19:12:55 +0000

    Prep3 complete

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,7 +3,30 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+    {   
+       Random randomGenerator = new Random();
+        int magic = randomGenerator.Next(1, 100);
+        int count = 0;
+        while (count != 1)
+        {
+        
+        Console.Write("What is your guess? ");
+        string num = Console.ReadLine();
+        int ans = int.Parse(num);
+        
+        if (ans > magic)
+        {
+            Console.WriteLine("Lower");
+        }
+        else if (ans < magic)
+        {
+            Console.WriteLine("Higher");
+        }
+        else
+        {
+            Console.WriteLine("You guessed it!");
+            count =1;
+        }
     }
+}
 }
