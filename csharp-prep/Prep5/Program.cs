16f508d20223005372f8279f29e1b7717545533c
erzrha <eatobrown@hotmail.com>
2023-01-12 21:53:46 +0000

    finished prep5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,36 @@ using System;
 
 class Program
 {
+    static void DisplayMessage()
+        {
+            Console.WriteLine("Welcome to the program!");
+            
+        }
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+    static string PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        string num = Console.ReadLine();
+        return num;
+    }
+    static int SquareNumber()
+    {
+        int total= int.Parse(PromptUserNumber()); 
+        return total*total;
+    }
+    static void DisplayResults()
+    {
+        
+        Console.Write($"{PromptUserName()}, the square of your number is {SquareNumber()}");
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayMessage();
+        DisplayResults();
     }
 }
