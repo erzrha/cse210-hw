5229274fbda2922fb0e196a242f5164c397c5e48
erzrha <eatobrown@hotmail.com>
2023-01-12 21:50:18 +0000

    Finished prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -3,7 +3,37 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep4 World!");
+    {   
+        int point = 0;
+        int high = 0;
+        List<int> num = new List<int>();
+        Console.Write("Enter number: ");
+        string my_num = Console.ReadLine();
+        int ans = int.Parse(my_num);
+        point +=ans;
+        num.Add(ans);
+        while (ans != 0)
+        {
+            Console.Write("Enter number: ");
+            my_num = Console.ReadLine();
+            ans = int.Parse(my_num);
+            point+=ans;
+            num.Add(ans);
+            if (ans> high)
+            {
+                high=ans;
+            }
+        }   
+        if (ans ==0)
+        {
+            Console.Write("The sum is: ");
+            Console.WriteLine(point);
+            Console.Write("The average is: ");
+            Console.WriteLine(point/num.Count);
+            Console.Write("The largest number is: ");
+            Console.WriteLine(high);
+            
+        }
     }
+
 }
