e7ad43f966080d6643ed63e2c83d29b7420e70f5
erzrha <eatobrown@hotmail.com>
2023-01-18 14:18:26 +0000

    Finished Learning 2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Head IT";
+        job1._company = "Microsoft";
+        job1._startYear = 2000;
+        job1._endYear = 2017;
+        
+
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2017;
+        job2._endYear = 2020;
+        
+        Resume my_Resume = new Resume(); 
+        my_Resume._jobs.Add(job1);
+        my_Resume._jobs.Add(job2);
+        my_Resume.DisplayResume();
+        
     }
 }
