24553f5fa422fc0755615849adee2fa698edde54
erzrha <eatobrown@hotmail.com>
2023-02-18 07:13:01 +0000

    Done

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        
+        MathsAssignment mathsAssignment = new MathsAssignment("Ezra Ato Brown", "Maths", "7.1", "8-19");
+        mathsAssignment.GetSummary();
+        mathsAssignment.GetHomeworkList();
+
+        WrittingAssignment writtingAssignment = new WrittingAssignment("Ezra Ato Brown", "European History", "The cause of World War II");
+        writtingAssignment.GetSummary();
+        writtingAssignment.GetWrittingInfo();
+
+
     }
 }
